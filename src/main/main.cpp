6f9b397b904f61141eab1302c63bd109b5d66fb8
Lukas Valine <lukas@valine.io>
2017-12-30 21:19:00 -0600

    Intial Cmake setup

---
+++ src/main/main.cpp
@@ -0,0 +1,34 @@
+#include "main.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <fstream>
+#include <string>
+
+// when testing, the main of gtest and this main function will collide,
+// this prevents the collision, but it's an ugly hack like all ifdef branches
+#ifdef UNIT_TESTS
+#define MAIN not_main
+#else
+#define MAIN main
+#endif
+
+int MAIN(int argc, char** argv) {
+
+
+  for (int i = 0; i < 100; i++) {
+
+    cout << i;
+  }
+  return EXIT_SUCCESS;
+}
+
+
+// just some function to have something that can be test
+#include <exception>
+
+void GetFrobnicationInterval() {
+  throw std::string("InvalidOperation: frobnication interval can't be retrieved");
+}
+
