badc50bf831fbb451ab8cf6fe1158c302fbc35fa
Lukas Valine <lukas@valine.io>
2018-01-08 00:37:25 -0600

    Working on linking glew

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -7,6 +7,12 @@ using std::endl;
 #include <fstream>
 #include <string>
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
 // when testing, the main of gtest and this main function will collide,
 // this prevents the collision, but it's an ugly hack like all ifdef branches
 #ifdef UNIT_TESTS
@@ -23,6 +29,45 @@ int MAIN(int argc, char** argv) {
   cin >> theNumber;
 
   cout << "Your number + 5 = " << theNumber + 5 << endl;
+
+  // start GL context and O/S window using the GLFW helper library
+  if (!glfwInit()) {
+    fprintf(stderr, "ERROR: could not start GLFW3\n");
+    return 1;
+  } 
+
+  // uncomment these lines if on Apple OS X
+  /*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
+
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
+  if (!window) {
+    fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+    glfwTerminate();
+    return 1;
+  }
+  glfwMakeContextCurrent(window);
+                                  
+  // start GLEW extension handler
+  glewExperimental = GL_TRUE;
+  glewInit();
+
+  // get version info
+  const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+  const GLubyte* version = glGetString(GL_VERSION); // version as a string
+  printf("Renderer: %s\n", renderer);
+  printf("OpenGL version supported %s\n", version);
+
+  // tell GL to only draw onto a pixel if the shape is closer to the viewer
+  glEnable(GL_DEPTH_TEST); // enable depth-testing
+  glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+
+  /* OTHER STUFF GOES HERE NEXT */
+  
+  // close GL context and any other GLFW resources
+  glfwTerminate();
   
   return EXIT_SUCCESS;
 }
--- src/main/main.h
+++ src/main/main.h
@@ -1,5 +1,6 @@
 #pragma once
 
+
 // returns the interval at which the program frobnicates
 // may also throw InvalidOperation of the frobnication interval can't be retrieved
 void GetFrobnicationInterval();
