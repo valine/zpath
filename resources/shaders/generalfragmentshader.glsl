976725148b7b00d84e7bc0631a0f094bf9312680
Lukas Valine <lukas@valine.io>
2018-02-22 09:48:58 -0600

    Removed gamma correction

--- resources/shaders/generalfragmentshader.glsl
+++ resources/shaders/generalfragmentshader.glsl
@@ -6,10 +6,10 @@ void main() {
     gl_FragColor = vec4(vColor, 1.0);
 
 
-    float gamma = 2.0;
-   	gl_FragColor.r = pow(gl_FragColor.r, 1.0/gamma);
-   	gl_FragColor.g = pow(gl_FragColor.g, 1.0/gamma);
-   	gl_FragColor.b = pow(gl_FragColor.b, 1.0/gamma);
+    //float gamma = 2.0;
+   	//gl_FragColor.r = pow(gl_FragColor.r, 1.0/gamma);
+   	//gl_FragColor.g = pow(gl_FragColor.g, 1.0/gamma);
+   	//gl_FragColor.b = pow(gl_FragColor.b, 1.0/gamma);
 }
 
 
--- src/main/ui/zlabel.cpp
+++ src/main/ui/zlabel.cpp
@@ -68,8 +68,8 @@ ZLabel::ZLabel(float maxWidth, float maxHeight, string font, string resourcePath
     FT_Done_Face(face);
     FT_Done_FreeType(ft);
 
-    //     // Configure VAO/VBO for texture quads
-     glGenVertexArrays(1, &VAO);
+        // Configure VAO/VBO for texture quads
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -78,8 +78,6 @@ ZLabel::ZLabel(float maxWidth, float maxHeight, string font, string resourcePath
     glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
-
-
 }
 
 void ZLabel::draw() {
