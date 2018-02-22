a9615bca0fb4ff3ebba29c2ef6b260e255b0ff9a
Lukas Valine <lukas@valine.io>
2018-02-22 12:00:51 -0600

    Added ambient lighting

--- resources/shaders/generalfragmentshader.glsl
+++ resources/shaders/generalfragmentshader.glsl
@@ -1,8 +1,3 @@
-
-
-
-
-
 varying vec3 vPosition;
 varying vec3 vNormal;
 
@@ -10,32 +5,30 @@ varying vec3 vNormal;
 
 void main() {
 
-	vec3 uLightPosition = vec3(4.0, 7.0, 0);
-	vec3 uLightColor = vec3(1.0, 1.0, 1.0);
-	float lightBrightness = 3.0;
-	vec4 uColor = vec4(1.0, 0.9, 0.9, 1.0);
 
-	float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * uLightColor;
+	vec4 uColor = vec4(1.0, 0.9, 0.9, 1.0);
+	
+	vec4 uAmbitentLightColor = vec4(0.031373, 0.156863, 0.278431, 1.000000);
+	float ambientStrength = 0.6;
+    vec3 ambient = ambientStrength * uAmbitentLightColor;
+
+    vec3 uLightPosition = vec3(4.0, 7.0, 0);
+	vec3 uLightColor = vec3(1.0, 1.0, 1.0);
+	float lightBrightness = 10.0;
 
 	vec3 norm = normalize(vNormal);
 	vec3 lightDir = normalize(uLightPosition - vPosition);  
 	
-	float attenuation = 2.2;
-	float distance = distance(uLightPosition, vPosition) / attenuation;
-	float diff = dot(norm, lightDir);
+	float attenuation = 2;
+	float distance = distance(uLightPosition, vPosition);
+	float diff = max(dot(norm, lightDir), 0.0);
 
-	vec3 diffuse = diff * uLightColor * lightBrightness / pow(distance, attenuation);
+	vec3 diffuse = diff * uLightColor;
 
-	vec3 result = diffuse * uColor.xyz;
+	vec3 result = (diffuse + ambient) * uColor.xyz;
 
     gl_FragColor = vec4(result, 1.0);
 
-
-    //float gamma = 2.0;
-   	//gl_FragColor.r = pow(gl_FragColor.r, 1.0/gamma);
-   	//gl_FragColor.g = pow(gl_FragColor.g, 1.0/gamma);
-   	//gl_FragColor.b = pow(gl_FragColor.b, 1.0/gamma);
 }
 
 
