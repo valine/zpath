7f69729c2c2d725e54fd7f6415def5f4583ed60e
Lukas Valine <lukas@valine.io>
2018-02-22 08:31:39 -0600

    Updated text shaders to support opengl 2

--- resources/shaders/textvertexshader.glsl
+++ resources/shaders/textvertexshader.glsl
@@ -1,7 +1,6 @@
 
-#version 330 core
-layout (location = 0) in vec4 vertex; // <vec2 pos, vec2 tex>
-out vec2 TexCoords;
+attribute vec4 vertex; // <vec2 pos, vec2 tex>
+varying vec2 TexCoords;
 
 uniform mat4 projection;
 
