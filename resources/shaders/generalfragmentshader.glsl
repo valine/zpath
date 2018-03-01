c883c95de49f3c0af816d6089262fc4eabb4ee8a
Lukas Valine <lukas@valine.io>
2018-03-01 10:27:28 -0600

    Added support for multiple lights

--- resources/shaders/generalfragmentshader.glsl
+++ resources/shaders/generalfragmentshader.glsl
@@ -3,39 +3,56 @@ varying vec3 vNormal;
 
 //uniform vec4 uColor;
 uniform vec3 uCameraPosition;
+
+vec3 pointLightPositions[] = {
+	vec3( 5.0,  0.2,  2.0),
+	vec3( 2.3, -12.3, -4.0),
+	vec3(-4.0,  2.0, -12.0),
+	vec3( 0.0,  0.0, -3.0)
+};  
+
+
 void main() {
 	vec4 uColor = vec4(1.0, 0.9, 0.9, 1.0);
+
 	
+	vec3 result = vec3(0.0, 0.0, 0.0);
 	// Ambitent light
 	vec3 uAmbitentLightColor = vec3(0.031373, 0.156863, 0.278431);
 	float ambientStrength = 0.5;
     vec3 ambient = vec3(ambientStrength) * uAmbitentLightColor;
 
+    for(int i = 0; i < 4; i++) {
+    	vec3 uLightColor = vec3(1.0, 1.0, 1.0);
+		float lightBrightness = 0.3;
+	    vec3 norm = normalize(vNormal);
+		vec3 lightDir = normalize(pointLightPositions[i] - vPosition);  
+
+	    // diffuse light
+		float attenuation = 1.0;
+		float distance = distance(pointLightPositions[i], vPosition);
+		float diff = max(dot(norm, lightDir), 0.0);
+
+		vec3 diffuse = diff * uLightColor * lightBrightness;
+
+		// spectular light 
+		float specularStrength = 1.0;
+		vec3 viewDir = normalize(uCameraPosition - vPosition);
+		vec3 reflectDir = reflect(-lightDir, norm);  
+		
+		float spec = pow(max(dot(viewDir, reflectDir), 0.0), 64.0);
+		vec3 specular = specularStrength * spec * uLightColor  * lightBrightness;
+
+		result += diffuse;
+		result += specular;  
+
+    }
     // light propertices 
-    vec3 uLightPosition = vec3(-7.0, 7.0, 5.0);
-	vec3 uLightColor = vec3(1.0, 1.0, 1.0);
-	float lightBrightness = 10.0;
-    vec3 norm = normalize(vNormal);
-	vec3 lightDir = normalize(uLightPosition - vPosition);  
-
-    // diffuse light
-	float attenuation = 1.0;
-	float distance = distance(uLightPosition, vPosition);
-	float diff = max(dot(norm, lightDir), 0.0);
-
-	vec3 diffuse = diff * uLightColor;
-
-	// spectular light 
-	float specularStrength = 1.0;
-	vec3 viewDir = normalize(uCameraPosition - vPosition);
-	vec3 reflectDir = reflect(-lightDir, norm);  
+    //vec3 uLightPosition = vec3(-7.0, 7.0, 5.0);
 	
-	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 64.0);
-	vec3 specular = specularStrength * spec * uLightColor;  
-
 	// result
 
-	vec3 result = (diffuse + ambient + specular) * uColor.xyz;
+	result = (result + ambient) * uColor.xyz;
 
     gl_FragColor = vec4(result, 1.0);
 }
