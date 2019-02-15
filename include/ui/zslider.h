01c988f74195e85452f205782c859f0107942cee
Lukas Valine <lukas@valine.io>
2019-02-15 05:49:10 -0600

    Fixed slider set value

--- include/ui/zslider.h
+++ include/ui/zslider.h
@@ -6,6 +6,7 @@
 #include "zview.h"
 #include "mesh/zpath_constants.h"
 #include "zsliderlistener.h"
+#include "zlabel.h"
 
 using namespace std;
 using namespace ZPathConstants;
@@ -17,6 +18,7 @@ class ZSlider : public ZView {
 
 public:
 	ZSlider(float maxWidth, float maxHeight, string title);
+	ZSlider(float maxWidth, float maxHeight, string title, string resourcePath);
 	void onKeyPress(int key, int scancode, int action, int mods);
 	void onMouseEvent(int button, int action, int mods, int x, int y);
 	void onCursorPosChange(double x, double y);
@@ -29,17 +31,21 @@ public:
 	void setValue(float value);
 	void setLineColor(vec4 color);
 	void setThumbColor(vec4 color);
+	void setIncrement(float);
 	
 private:
 	ZView* mThumb;
 	ZView* mLine;
+	string mTitle;
 
 	float mMaxValue = 1.0;
 	float mMinValue = 0.0;
 
 	ZSliderListener* mListener = nullptr;
+	ZLabel* mLabel;
+	float mIncrement = 0.01;
 
-	void valueChanged(int offset);
+	void valueChanged(float offset);
 };
 
 #endif
--- src/main/ui/zslider.cpp
+++ src/main/ui/zslider.cpp
@@ -1,12 +1,12 @@
 #include "ui/zslider.h"
 
-ZSlider::ZSlider(float maxWidth, float maxHeight, string title): 
+ZSlider::ZSlider(float maxWidth, float maxHeight, string label): 
 ZView(maxWidth, maxHeight) {
 	
 	float lineColor[4] = {0.0, 0.0, 0.0, 1.0};
 	float thumbColor[4] = {0.1, 0.2, 0.9, 1.0};
 
-	float lineHeight = 2;
+	float lineHeight = SLIDER_THUMB_SIZE + 2;
 	mLine = new ZView(getWidth(), lineHeight);
 	mLine->setOffset(0,getHeight() / 2 - (lineHeight / 2));
 	mLine->setBackgroundColor(lineColor);
@@ -16,7 +16,22 @@ ZView(maxWidth, maxHeight) {
 	mThumb->setBackgroundColor(thumbColor);
 	mThumb->setOffset(0,getHeight() / 2 - (SLIDER_THUMB_SIZE / 2));
 	addSubView(mThumb);
+
+	mTitle = label;
 }
+
+
+ZSlider::ZSlider(float maxWidth, float maxHeight, string title, string resourcePath) : 
+ZSlider(maxWidth, maxHeight, title) {
+	
+	mLabel = new ZLabel(maxWidth, 18, "roboto/Roboto-Medium.ttf", resourcePath);
+    mLabel->setOffset(0,0);
+    mLabel->setTextColor(vec4(1.0));
+    mLabel->setGravity(ZView::topLeft);
+    addSubView(mLabel);
+    setValue(15);
+}
+
 
 void ZSlider::onMouseEvent(int button, int action, int mods, int x, int y) {
 	ZView::onMouseEvent(button, action, mods, x, y);
@@ -25,6 +40,11 @@ void ZSlider::onMouseEvent(int button, int action, int mods, int x, int y) {
 
  	}
 }
+
+void ZSlider::setIncrement(float increment) {
+	mIncrement = increment;
+}
+
 
 void ZSlider::onKeyPress(int key, int scancode, int action, int mods) {
 	ZView::onKeyPress(key, scancode, action, mods);
@@ -77,8 +97,16 @@ void ZSlider::valueChanged(int offset) {
 	float factor = (float) offset / maxOffset;
 	float value = ((mMaxValue - mMinValue) * factor) + mMinValue;
 	
+	float incValue = roundf(value / mIncrement) * mIncrement;
+
+	auto format = "%.2f";
+	auto size = std::snprintf(nullptr, 0, format, incValue);
+	std::string output(size + 1, '\0');
+	std::sprintf(&output[0], format, incValue);
+
+	mLabel->setText(mTitle + " " + output);
 	if (mListener != nullptr) {
-		mListener->onSliderValueChanged(this, value);
+		mListener->onSliderValueChanged(this, incValue);
 	}
 }
 
@@ -91,9 +119,20 @@ void ZSlider::setMinValue(float min) {
 }
 
 void ZSlider::setValue(float value) {
-	int offset = (mMinValue - value) / (-mMaxValue + mMinValue);
+	float maxOffset = (float) (getWidth() - SLIDER_THUMB_SIZE);
+	float factor = (value - mMinValue) / (mMaxValue - mMinValue);
+	float offset = factor * maxOffset;
 	int yPosition = getHeight() / 2 - (SLIDER_THUMB_SIZE / 2);
-	mThumb->setOffset(offset, yPosition);
+
+	float newOffset= offset;
+	if (newOffset < 0) {
+		newOffset = 0;
+	} else if (newOffset > getWidth() - SLIDER_THUMB_SIZE) {
+		newOffset = getWidth() - SLIDER_THUMB_SIZE;
+	}
+
+	mThumb->setOffset(newOffset, yPosition);
+	valueChanged(newOffset);
 }
 
 void ZSlider::setLineColor(vec4 color) {
