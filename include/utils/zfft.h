3d5759c750d9ab5d74d8cdba0d231a7b6527f846
Lukas Valine <lukas@valine.io>
2020-01-24 00:06:43 -0600

    Update FFT

--- include/utils/zfft.h
+++ include/utils/zfft.h
@@ -3,6 +3,7 @@
 #define ZPATH_ZFFTUTIL_H
 
 #include <string>
+#include <complex.h>
 #include "ui/z3dview.h"
 
 using namespace std;
@@ -12,6 +13,16 @@ class ZFFt {
 public:
     void calculate(double* data, unsigned long nn);
 
+    void convolve(const vector<std::complex<double>> &xvec, const vector<std::complex<double>> &yvec,
+                  vector<std::complex<double>> &outvec);
+
+    void transformBluestein(vector<std::complex<double>> &vec);
+
+    void transformRadix2(vector<std::complex<double>> &vec);
+
+    void inverseTransform(vector<std::complex<double>> &vec);
+
+    void transform(vector<std::complex<double>> &vec);
 };
 
 
--- src/main/utils/zfft.cpp
+++ src/main/utils/zfft.cpp
@@ -14,51 +14,164 @@
 #include <pwd.h>
 #include "png.h"
 
+/*
+ * Free FFT and convolution (C++)
+ *
+ * Copyright (c) 2019 Project Nayuki. (MIT License)
+ * https://www.nayuki.io/page/free-small-fft-in-multiple-languages
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * - The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ * - The Software is provided "as is", without warranty of any kind, express or
+ *   implied, including but not limited to the warranties of merchantability,
+ *   fitness for a particular purpose and noninfringement. In no event shall the
+ *   authors or copyright holders be liable for any claim, damages or other
+ *   liability, whether in an action of contract, tort or otherwise, arising from,
+ *   out of or in connection with the Software or the use or other dealings in the
+ *   Software.
+ */
 
-void ZFFt::calculate(double* data, unsigned long nn) {
-    unsigned long n, mmax, m, j, istep, i;
-    double wtemp, wr, wpr, wpi, wi, theta;
-    double tempr, tempi;
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <stdexcept>
+#include <complex.h>
 
-    n = nn<<1;
-    j=1;
-    for (i=1; i<n; i+=2) {
-        if (j>i) {
-            swap(data[j-1], data[i-1]);
-            swap(data[j], data[i]);
-        }
-        m = nn;
-        while (m>=2 && j>m) {
-            j -= m;
-            m >>= 1;
-        }
-        j += m;
-    };
+using std::complex;
+using std::size_t;
+using std::vector;
 
-    mmax=2;
-    while (n>mmax) {
-        istep = mmax<<1;
-        theta = -(2*M_PI/mmax);
-        wtemp = sin(0.5*theta);
-        wpr = -2.0*wtemp*wtemp;
-        wpi = sin(theta);
-        wr = 1.0;
-        wi = 0.0;
-        for (m=1; m < mmax; m += 2) {
-            for (i=m; i <= n; i += istep) {
-                j=i+mmax;
-                tempr = wr*data[j-1] - wi*data[j];
-                tempi = wr * data[j] + wi*data[j-1];
 
-                data[j-1] = data[i-1] - tempr;
-                data[j] = data[i] - tempi;
-                data[i-1] += tempr;
-                data[i] += tempi;
-            }
-            wtemp=wr;
-            wr += wr*wpr - wi*wpi;
-            wi += wi*wpr + wtemp*wpi;
-        }
-        mmax=istep;
+// Private function prototypes
+static size_t reverseBits(size_t x, int n);
+
+
+void ZFFt::transform(vector<complex<double> > &vec) {
+    size_t n = vec.size();
+    if (n == 0)
+        return;
+    else if ((n & (n - 1)) == 0)  // Is power of 2
+        transformRadix2(vec);
+    else  // More complicated algorithm for arbitrary sizes
+        transformBluestein(vec);
+}
+
+
+void ZFFt::inverseTransform(vector<complex<double> > &vec) {
+    std::transform(vec.cbegin(), vec.cend(), vec.begin(),
+                   static_cast<complex<double> (*)(const complex<double> &)>(std::conj));
+    transform(vec);
+    std::transform(vec.cbegin(), vec.cend(), vec.begin(),
+                   static_cast<complex<double> (*)(const complex<double> &)>(std::conj));
+}
+
+
+void ZFFt::transformRadix2(vector<complex<double> > &vec) {
+    // Length variables
+    size_t n = vec.size();
+    int levels = 0;  // Compute levels = floor(log2(n))
+    for (size_t temp = n; temp > 1U; temp >>= 1)
+        levels++;
+    if (static_cast<size_t>(1U) << levels != n)
+        throw std::domain_error("Length is not a power of 2");
+
+    // Trignometric table
+    vector<complex<double> > expTable(n / 2);
+    for (size_t i = 0; i < n / 2; i++)
+        expTable[i] = std::polar(1.0, -2 * M_PI * i / n);
+
+    // Bit-reversed addressing permutation
+    for (size_t i = 0; i < n; i++) {
+        size_t j = reverseBits(i, levels);
+        if (j > i)
+            std::swap(vec[i], vec[j]);
     }
+
+    // Cooley-Tukey decimation-in-time radix-2 FFT
+    for (size_t size = 2; size <= n; size *= 2) {
+        size_t halfsize = size / 2;
+        size_t tablestep = n / size;
+        for (size_t i = 0; i < n; i += size) {
+            for (size_t j = i, k = 0; j < i + halfsize; j++, k += tablestep) {
+                complex<double> temp = vec[j + halfsize] * expTable[k];
+                vec[j + halfsize] = vec[j] - temp;
+                vec[j] += temp;
+            }
+        }
+        if (size == n)  // Prevent overflow in 'size *= 2'
+            break;
+    }
+}
+
+
+void ZFFt::transformBluestein(vector<complex<double> > &vec) {
+    // Find a power-of-2 convolution length m such that m >= n * 2 + 1
+    size_t n = vec.size();
+    size_t m = 1;
+    while (m / 2 <= n) {
+        if (m > SIZE_MAX / 2)
+            throw std::length_error("Vector too large");
+        m *= 2;
+    }
+
+    // Trignometric table
+    vector<complex<double> > expTable(n);
+    for (size_t i = 0; i < n; i++) {
+        unsigned long long temp = static_cast<unsigned long long>(i) * i;
+        temp %= static_cast<unsigned long long>(n) * 2;
+        double angle = M_PI * temp / n;
+        expTable[i] = std::polar(1.0, -angle);
+    }
+
+    // Temporary vectors and preprocessing
+    vector<complex<double> > av(m);
+    for (size_t i = 0; i < n; i++)
+        av[i] = vec[i] * expTable[i];
+    vector<complex<double> > bv(m);
+    bv[0] = expTable[0];
+    for (size_t i = 1; i < n; i++)
+        bv[i] = bv[m - i] = std::conj(expTable[i]);
+
+    // Convolution
+    vector<complex<double> > cv(m);
+    convolve(av, bv, cv);
+
+    // Postprocessing
+    for (size_t i = 0; i < n; i++)
+        vec[i] = cv[i] * expTable[i];
+}
+
+
+void ZFFt::convolve(
+        const vector<complex<double> > &xvec,
+        const vector<complex<double> > &yvec,
+        vector<complex<double> > &outvec) {
+
+    size_t n = xvec.size();
+    if (n != yvec.size() || n != outvec.size())
+        throw std::domain_error("Mismatched lengths");
+    vector<complex<double> > xv = xvec;
+    vector<complex<double> > yv = yvec;
+    transform(xv);
+    transform(yv);
+    for (size_t i = 0; i < n; i++)
+        xv[i] *= yv[i];
+    inverseTransform(xv);
+    for (size_t i = 0; i < n; i++)  // Scaling (because this FFT implementation omits it)
+        outvec[i] = xv[i] / static_cast<double>(n);
+}
+
+
+static size_t reverseBits(size_t x, int n) {
+    size_t result = 0;
+    for (int i = 0; i < n; i++, x >>= 1)
+        result = (result << 1) | (x & 1U);
+    return result;
 }
