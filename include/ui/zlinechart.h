c3c717860870b38dd30c26a38fe151931c374a52
Lukas Valine <lukas@valine.io>
2020-12-05 23:02:29 -0600

    Added line chart class

---
+++ include/ui/zlinechart.h
@@ -0,0 +1,21 @@
+//
+// Created by lukas on 12/5/20.
+//
+
+#ifndef ZPATH_ZLINECHART_H
+#define ZPATH_ZLINECHART_H
+
+
+#include "zview.h"
+
+class ZLineChart : public ZView {
+
+public:
+    ZLineChart(float width, float height, ZView* parent);
+
+private:
+
+};
+
+
+#endif //ZPATH_ZLINECHART_H
---
+++ src/main/ui/chart/zlinechart.cpp
@@ -0,0 +1,9 @@
+//
+// Created by lukas on 12/5/20.
+//
+
+#include "ui/zlinechart.h"
+
+ZLineChart::ZLineChart(float width, float height, ZView *parent) : ZView(width, height, parent) {
+
+}
