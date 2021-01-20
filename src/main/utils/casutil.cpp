c39bb6d62445fb044646e3a76db9230405ebfe32
Lukas Valine <lukas@valine.io>
2021-01-19 23:13:48 -0600

    Expression tokens work

--- src/main/utils/casutil.cpp
+++ src/main/utils/casutil.cpp
@@ -6,6 +6,10 @@
 #include <giac/config.h>
 #include <giac/giac.h>
 
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
 using namespace std;
 using namespace giac;
 
@@ -23,14 +27,23 @@ void CasUtil::testCompute() {
     cout << e << endl;
 
     string testString = "3 + 4(10+2*2)+1";
+    testString.erase(std::remove_if(testString.begin(), testString.end(), ::isspace), testString.end());
+
     char testArray[testString.length() + 1];
     strcpy(testArray, testString.c_str());
+    char *copy = strdup(testArray);
 
     const char* delim = " +-*/()";
     char* afterSplit = strtok(testArray, delim);
 
     while (afterSplit != nullptr) {
-        printf ("%s\n", afterSplit);
+
+        char d = copy[afterSplit - testArray + strlen(afterSplit)];
+        cout << "t:" << afterSplit << endl;
+        cout << "d:" << d << endl;
+
+
+
         afterSplit = strtok(nullptr, delim);
     }
 
