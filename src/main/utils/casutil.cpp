61ecb27618a9457a16bb39451d2157f2172fc287
Lukas Valine <lukas@valine.io>
2021-01-19 22:34:05 -0600

    Working on tolkenizer

--- src/main/utils/casutil.cpp
+++ src/main/utils/casutil.cpp
@@ -16,8 +16,24 @@ void CasUtil::testCompute() {
     gen e("laplace(sin(x))", &ct);
     e = eval(e, 1, &ct);
 
-
     string out;
     stringstream stream(out);
     stream << e << endl;
+
+    cout << e << endl;
+
+    string testString = "3 + 4(10+2*2)+1";
+    char testArray[testString.length() + 1];
+    strcpy(testArray, testString.c_str());
+
+    const char* delim = " +-*/()";
+    char* afterSplit = strtok(testArray, delim);
+
+    while (afterSplit != nullptr) {
+        printf ("%s\n", afterSplit);
+        afterSplit = strtok(nullptr, delim);
+    }
+
+
+    /// symbols test
 }
