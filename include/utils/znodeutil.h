da3634b6d317d0f4be5377f95e309f4308040ee4
Lukas Valine <lukas@valine.io>
2021-01-24 22:43:19 -0600

    Fixed issue with tokens

--- include/utils/znodeutil.h
+++ include/utils/znodeutil.h
@@ -26,15 +26,22 @@ public:
     vector<ZNodeView *> stringToGraph(string input) {
 
 
-        string testString = "3 + 4(10+2*2)+1";
+        string testString = "3 + 4(10.0014 +2*2)+ max(1,2)";
 
 
         vector<string> tokens = getTokens(testString);
 
-
         stack<string> operators;
         queue<string> outputs;
 
+
+        // Shunting-yard implementation
+        for (string token : tokens) {
+            if (isNumber(token)) {
+                outputs.push(token);
+            }
+
+        }
 
 
         return vector<ZNodeView*>();
@@ -49,12 +56,12 @@ public:
         strcpy(testArray, input.c_str());
         char *copy = strdup(testArray);
 
-        const char* delim = " +-*/^()";
+        const char* delim = " ,+-*/^()";
         char* afterSplit = strtok(testArray, delim);
         char d = copy[afterSplit - testArray + strlen(afterSplit)];
 
+       // tokens.emplace_back(string(1, d));
         tokens.emplace_back(string(afterSplit));
-        tokens.emplace_back(string(1, d));
 
         while (afterSplit != nullptr) {
 
@@ -64,19 +71,97 @@ public:
             afterSplit = strtok(nullptr, delim);
 
             if (afterSplit != nullptr) {
-                tokens.emplace_back(string(afterSplit));
                 tokens.emplace_back(string(1, d));
+                tokens.emplace_back(string(afterSplit));
+
             }
         }
 
         return tokens;
     }
 
-    bool isNumber(const std::string& s) {
-        return !s.empty() && std::find_if(s.begin(),
-                s.end(), [](unsigned char c) {
-            return !std::isdigit(c);
-        }) == s.end();
+    static bool isNumber(const std::string& str) {
+        int i = 0, j = str.length() - 1;
+
+        // Handling whitespaces
+        while (i < str.length() && str[i] == ' ') {
+            i++;
+        }
+        while (j >= 0 && str[j] == ' ') {
+            j--;
+        }
+
+        if (i > j) {
+            return false;
+        }
+
+        if (i == j && !(str[i] >= '0' && str[i] <= '9')) {
+            return false;
+        }
+
+        if (str[i] != '.' && str[i] != '+'
+            && str[i] != '-' && !(str[i] >= '0' && str[i] <= '9')) {
+            return false;
+        }
+
+        // To check if a '.' or 'e' is found in given
+        // string. We use this flag to make sure that
+        // either of them appear only once.
+        bool flagDotOrE = false;
+
+        for (i; i <= j; i++) {
+            // If any of the char does not belong to
+            // {digit, +, -, ., e}
+            if (str[i] != 'e' && str[i] != '.'
+                && str[i] != '+' && str[i] != '-'
+                && !(str[i] >= '0' && str[i] <= '9')) {
+                return false;
+            }
+
+            if (str[i] == '.') {
+                // checks if the char 'e' has already
+                // occurred before '.' If yes, return 0.
+                if (flagDotOrE) {
+                    return false;
+                }
+
+                // If '.' is the last character.
+                if (i + 1 > str.length()) {
+                    return false;
+                }
+
+                // if '.' is not followed by a digit.
+                if (!(str[i + 1] >= '0' && str[i + 1] <= '9')) {
+                    return false;
+                }
+            }
+
+            else if (str[i] == 'e') {
+                // set flagDotOrE = 1 when e is encountered.
+                flagDotOrE = true;
+
+                // if there is no digit before 'e'.
+                if (!(str[i - 1] >= '0' && str[i - 1] <= '9')) {
+                    return false;
+                }
+
+                // If 'e' is the last Character
+                if (i + 1 > str.length()) {
+                    return false;
+                }
+
+                // if e is not followed either by
+                // '+', '-' or a digit
+                if (str[i + 1] != '+' && str[i + 1] != '-'
+                    && (str[i + 1] >= '0' && str[i] <= '9')) {
+                    return false;
+                }
+            }
+        }
+
+        /* If the string skips all above cases, then
+        it is numeric*/
+        return true;
     }
 };
 
