94639fc9d6379acdd86b8526e6388f6547ea1c9d
Lukas Valine <lukas@valine.io>
2021-02-18 20:45:09 -0600

    Working on implementing view shadows

---
+++ include/utils/zshadowrenderer.h
@@ -0,0 +1,26 @@
+//
+// Created by lukas on 2/18/21.
+//
+
+#ifndef ZPATH_ZSHADOWRENDERER_H
+#define ZPATH_ZSHADOWRENDERER_H
+
+
+#include <ui/ztexture.h>
+
+class ZShadowRenderer {
+
+    unsigned int mFBO;
+    unsigned int mVAO;
+
+
+    ZTexture* createShadow() {
+
+        
+        return nullptr;
+    }
+
+};
+
+
+#endif //ZPATH_ZSHADOWRENDERER_H
---
+++ src/main/utils/zshadowrenderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lukas on 2/18/21.
+//
+
+#include "utils/zshadowrenderer.h"
