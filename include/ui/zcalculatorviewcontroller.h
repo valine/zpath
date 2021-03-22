9a8046b86bedf534e9e0b74f4af9ce348fa43c94
Lukas Valine <lukas@valine.io>
2021-03-21 22:59:32 -0500

    Added calculator view controller

---
+++ include/ui/zcalculatorviewcontroller.h
@@ -0,0 +1,17 @@
+//
+// Created by Lukas Valine on 3/21/21.
+//
+
+#ifndef ZPATH_ZCALCULATORVIEWCONTROLLER_H
+#define ZPATH_ZCALCULATORVIEWCONTROLLER_H
+
+#include "zviewcontroller.h"
+
+class ZCalculatorViewController : public ZViewController {
+
+
+    void onCreate() override;
+};
+
+
+#endif //ZPATH_ZCALCULATORVIEWCONTROLLER_H
---
+++ src/main/ui/nodeEditor/zcalculatorviewcontroller.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lukas Valine on 3/21/21.
+//
+
+#include "ui/zcalculatorviewcontroller.h"
