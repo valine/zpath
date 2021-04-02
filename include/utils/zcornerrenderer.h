705e762fda2af2fea59be0c58c23dcf234e72957
Lukas Valine <lukas@valine.io>
2021-04-01 22:40:09 -0500

    Added corner renderer

---
+++ include/utils/zcornerrenderer.h
@@ -0,0 +1,28 @@
+//
+// Created by Lukas Valine on 4/1/21.
+//
+
+#ifndef ZPATH_ZCORNERRENDERER_H
+#define ZPATH_ZCORNERRENDERER_H
+
+
+class ZCornerRenderer {
+
+
+public:
+
+    void draw() {
+
+
+    }
+
+private:
+
+
+    void init() {
+
+    }
+};
+
+
+#endif //ZPATH_ZCORNERRENDERER_H
---
+++ src/main/utils/zcornerrenderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lukas Valine on 4/1/21.
+//
+
+#include "utils/zcornerrenderer.h"
