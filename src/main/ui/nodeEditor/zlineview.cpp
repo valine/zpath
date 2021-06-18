02c29072bdedf68fb402090bafd365605d421512
Lukas Valine <lukas@valine.io>
2021-06-17 23:51:23 -0500

    Fixed line drawing on low dp monitors

--- src/main/ui/nodeEditor/zlineview.cpp
+++ src/main/ui/nodeEditor/zlineview.cpp
@@ -8,13 +8,18 @@
 ZLineView::ZLineView(vec2 point1, vec2 point2, ZView *parent): ZView(fillParent, fillParent, parent)  {
     mPoint1 = point1;
     mPoint2 = point2;
-
     init();
 }
 
 void ZLineView::init() {
     setBackgroundColor(ZSettingsStore::getInstance().getHighlightColor());
-    setOutlineType(none);
+
+    if (mDP > 1.0) {
+        setOutlineType(none);
+    } else {
+        setOutlineType(full);
+    }
+
 }
 
 void ZLineView::setPoints(vec2 point1, vec2 point2) {
@@ -32,23 +37,39 @@ void ZLineView::computeBounds() {
 
     float* vertices = getVertices();
 
-    float angle = atan2((mPoint2.y - mPoint1.y), (mPoint2.x - mPoint1.x));
-    float lineWidth = 0.75 * mDP;
-    float xOffset = sin(angle) * lineWidth;
-    float yOffset = cos(angle) * lineWidth;
+    // If dp is higher than 1 use plane instead of line. This
+    // line is better for low DP due to antialiasing, but doesn't
+    // support larger line width on macOS.
+    if (mDP > 1.0) {
+        float angle = atan2((mPoint2.y - mPoint1.y), (mPoint2.x - mPoint1.x));
+        float lineWidth = 1.0;
+        float xOffset = sin(angle) * lineWidth;
+        float yOffset = cos(angle) * lineWidth;
 
-    vertices[0] = mPoint1.x + xOffset;
-    vertices[1] = mPoint1.y - yOffset;
+        vertices[0] = mPoint1.x + xOffset;
+        vertices[1] = mPoint1.y - yOffset;
 
-    vertices[4] = mPoint2.x + xOffset;
-    vertices[5] = mPoint2.y - yOffset;;
+        vertices[4] = mPoint2.x + xOffset;
+        vertices[5] = mPoint2.y - yOffset;
 
-    vertices[8] =  mPoint1.x - xOffset;
-    vertices[9] =  mPoint1.y + yOffset;
+        vertices[8] =  mPoint1.x - xOffset;
+        vertices[9] =  mPoint1.y + yOffset;
 
-    vertices[12] = mPoint2.x - xOffset;
-    vertices[13] = mPoint2.y + yOffset;
+        vertices[12] = mPoint2.x - xOffset;
+        vertices[13] = mPoint2.y + yOffset;
+    } else {
+        vertices[0] = mPoint1.x;
+        vertices[1] = mPoint1.y;
 
+        vertices[4] = mPoint2.x;
+        vertices[5] = mPoint2.y;
+
+        vertices[8] =  mPoint1.x;
+        vertices[9] =  mPoint1.y;
+
+        vertices[12] = mPoint2.x;
+        vertices[13] = mPoint2.y;
+    }
 
     invalidate();
 }
