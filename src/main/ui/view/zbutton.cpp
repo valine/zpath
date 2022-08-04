e5757aed32412b739c15ecc003e12c32e622a8b9
Lukas Valine <lukas@valine.io>
2022-08-04 15:20:50 -0500

    Fixed button highlight issue

--- src/main/ui/view/zbutton.cpp
+++ src/main/ui/view/zbutton.cpp
@@ -45,8 +45,8 @@ void ZButton::onMouseEvent(int button, int action, int mods, int x, int y) {
 	ZView::onMouseEvent(button, action, mods, x, y);
 
 	vec4 highlight = vec4(0.1, 0.1, 0.1, 0);
-
 	if (action == GLFW_PRESS) {
+        mOriginalBackground = getBackgroundColor();
 		setBackgroundColor(getBackgroundColor() - highlight);
 		showHighlight(false);
 		mWasPressed = true;
@@ -55,9 +55,9 @@ void ZButton::onMouseEvent(int button, int action, int mods, int x, int y) {
 
 	if (action == GLFW_RELEASE) {
 		if (mWasPressed || mClickMode == upAndDown) {
-
 		    if (mWasPressed) {
-                setBackgroundColor(getBackgroundColor() + highlight);
+                setBackgroundColor(mOriginalBackground);
+                invalidate();
             }
 
             if (mOnClickSimple != nullptr) {
