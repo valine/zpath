838305033bff50b662be14288ff355a6e50e32dc
Lukas Valine <lukas@valine.io>
2023-07-15 22:22:45 -0500

    Load more characters

--- src/main/utils/zfontstore.cpp
+++ src/main/utils/zfontstore.cpp
@@ -41,8 +41,8 @@ FT_Face ZFontStore::loadFont(string resourcePath, float dp, int size) {
         // Disable byte-alignment restriction
         glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-        // Load first 128 characters of ASCII set
-        for (GLubyte c = 0; c < 255; c++) {
+        // Load first 1024 characters of ASCII set
+        for (GLuint c = 0; c < 1024; c++) {
             // Load character glyph
             if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
                 std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
